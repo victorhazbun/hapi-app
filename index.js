a01ea2c9a5fca7d71c6b3a0ca7ddbaf101539618
victor hazbun <victorhazbun87@gmail.com>
2017-02-09 21:45:53 -0500

    Nice index with style

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 'use strict';
 
-var Hapi = require('hapi'),
-server = new Hapi.Server();
+const Path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
+});
 
 server.connection({port: 3000});
 
+server.register(Inert, () => {});
+
+server.ext('onRequest', (request, reply) => {
+  console.log(`Request received: ${request.path}`);
+  reply.continue();
+});
+
 server.route({
-  path: '/hi',
+  path: '/',
   method: 'GET',
-  handler: function(request, reply) {
-    return reply('hi');
+  handler: {
+    file: 'templates/index.html'
   }
 });
 
-server.start(function(){
-  console.log('Listening...')
+server.route({
+    method: 'GET',
+    path: '/assets/{path*}',
+    handler: {
+      directory: {
+          path: '.',
+          redirectToSlash: true,
+          index: true
+      }
+    }
+});
+
+server.start((err) => {
+
+  if (err) {
+      throw err;
+  }
+
+  console.log('Server running at:', server.info.uri);
 });
