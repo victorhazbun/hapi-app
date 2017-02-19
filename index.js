9433ff6c54cb32c4fd5e33ccb31973a470c0d819
victor hazbun <victorhazbun87@gmail.com>
2017-02-18 21:28:29 -0500

    Organize code

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict';
-
-const uuid = require('uuid');
-const Path = require('path');
 const Hapi = require('hapi');
 const Inert = require('inert');
-const fs = require('fs');
-const Joi = require('joi');
-const Boom = require('boom');
+const Path = require('path');
+const CardStore = require('./lib/card-store');
+
 const server = new Hapi.Server({
   connections: {
     routes: {
@@ -17,61 +14,7 @@ const server = new Hapi.Server({
   }
 });
 
-var cardSchema = Joi.object().keys({
-  name: Joi.string().min(3).max(50).required(),
-  recipientEmail: Joi.string().email().required(),
-  senderName: Joi.string().min(3).max(50).required(),
-  senderEmail: Joi.string().email().required(),
-  cardImage: Joi.string().regex(/.+\.(jpg|bmp|png|gif)\b/).required()
-});
-
-function loadCards() {
-  var file = fs.readFileSync('./cards.json');
-  return JSON.parse(file.toString());
-}
-
-function mapImages() {
-  return fs.readdirSync('./public/images/cards');
-}
-
-let cards = loadCards();
-
-function saveCard(card) {
-  let id = uuid.v1();
-  card.id = id;
-  cards[id] = card;
-}
-
-function newCardHandler(request, reply) {
-  if (request.method === 'get') {
-    reply.view('new', { card_images: mapImages() });
-  } else {
-    Joi.validate(request.payload, cardSchema, function(err, val) {
-      if (err) {
-        return reply(Boom.badRequest(err.details[0].message));
-      }
-      var card = {
-        name: val.name,
-        recipientEmail: val.recipientEmail,
-        senderName: val.senderName,
-        senderEmail: val.senderEmail,
-        cardImage: val.cardImage
-      };
-      saveCard(card);
-      reply.redirect('/cards');
-    });
-
-  }
-}
-
-function cardsHandler(request, reply) {
-  reply.view('cards', {cards: cards});
-}
-
-function deleteCardHandler(request, reply) {
-  delete cards[request.params.id];
-  reply();
-}
+CardStore.initialize();
 
 server.connection({port: 3000});
 
@@ -79,50 +22,13 @@ server.register(require('vision'), (err) => {
   if (err) {
     throw err;
   }
-
   server.views({
     engines: { html: require('handlebars') },
     path: __dirname + '/public/templates'
   });
 
-  server.route({
-    path: '/',
-    method: 'GET',
-    handler: {
-      file: 'templates/index.html'
-    }
-  });
 
-  server.route({
-      method: 'GET',
-      path: '/assets/{path*}',
-      handler: {
-        directory: {
-            path: '.',
-            redirectToSlash: true,
-            index: true
-        }
-      }
-  });
-
-  server.route({
-    path: '/cards/new',
-    method: ['GET', 'POST'],
-    handler: newCardHandler
-  });
-
-  server.route({
-    path: '/cards',
-    method: 'GET',
-    handler: cardsHandler
-  });
-
-  server.route({
-    path: '/cards/{id}',
-    method: 'DELETE',
-    handler: deleteCardHandler
-  });
-
+  server.route(require('./lib/routes'));
 });
 
 server.register(Inert, () => {});
